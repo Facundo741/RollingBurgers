fc17add50426b950419d4337e8e309ed1795c92c
Facundo <santillanfacundo43@gmail.com>
2023-11-09 02:23:46 -0300

    Update cargando todo

--- Components/NavBar.js
+++ Components/NavBar.js
@@ -1,36 +1,46 @@
+// navbar.js
+
 const header = document.querySelector('header');
 
-const navbar = () => {
-
+const createNavbar = () => {
     const user = JSON.parse(localStorage.getItem('userLog'));
 
-
-    console.log(user);
-    return (header.innerHTML = `
-            <nav class="navbar navbar-expand-lg bg-body-tertiary ">
-            <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                </li>
-                
-                ${user.rol === "administrador" ?  '<li class="nav-item"><a class="nav-link" href="/Admin/Admin.html">Administracion</a></li>': ' hola'}
-                </ul>
-            </div>
+    header.innerHTML = `
+        <nav class="navbar navbar-expand-lg bg-black ">
+            <div class="container-fluid text-center h-100">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <iconify-icon icon="openmoji:hamburger" width="70" height="70"></iconify-icon>
+                </button>
+                <div class="collapse navbar-collapse " id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active text-light" aria-current="page" href="../Principal/index.html">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="../About/aboutme.html">Acerca de</a>
+                        </li>
+                        ${user ? '' :
+                            '<li class="nav-item "><a class="nav-link text-light " id="iniciarSesionLink" href="../Login/login.html">Iniciar Sesión</a></li>' }
+                        ${user ? 
+                            '<li class="nav-item"><a class="nav-link text-light" id="cerrarSesionLink" href="../Principal/index.html">Cerrar Sesión</a></li>' : ''}
+                        
+                        ${user && user.rol === "administrador" ? 
+                            '<li class="nav-item"><a class="nav-link text-light" href="/Admin/Admin.html">Administracion</a></li>' : ''}
+                    </ul>
+                </div>
             </div>
         </nav>
-    `);
+    `;
+    const cerrarSesionLink = document.getElementById('cerrarSesionLink');
+    if (cerrarSesionLink) {
+        cerrarSesionLink.addEventListener('click', function (e) {
+            e.preventDefault(); 
+            localStorage.clear();
+            window.location.href = '../Principal/index.html';
+        });
+    }
 }
 
-export default navbar;
+document.addEventListener('DOMContentLoaded', createNavbar);
+
+export default createNavbar;
